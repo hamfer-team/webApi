f8fbee22836593ccd8e37eaaab7ea5ea6b5a614c
seyyed habibolah Tabasi <Tabasi@DESKTOP-7338EO5>
2026-02-07 21:05:20 +0330

    Extend by `WebApiTextContentResult` & `WebApiObjectContentResult`

--- src/Server/Models/WebApiContentResult.cs
+++ src/Server/Models/WebApiContentResult.cs
@@ -1,7 +1,29 @@
+using System.Text.Json;
+using Hamfer.WebApi.Core;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Hamfer.WebApi.Server.Models;
 
 public class WebApiContentResult : ContentResult, IWebApiActionResult
 {
+}
+
+public class WebApiTextContentResult : ContentResult, IWebApiActionResult
+{
+  public WebApiTextContentResult(string content, WebApiStatusCode statusCode)
+  {
+    this.ContentType = WebApiContentType.TEXT;
+    this.Content = content;
+    this.StatusCode = (int)statusCode;
+  }
+}
+
+public class WebApiObjectContentResult<TObject> : ContentResult, IWebApiActionResult
+{
+  public WebApiObjectContentResult(TObject content, WebApiStatusCode statusCode)
+  {
+    this.ContentType = WebApiContentType.JSON;
+    this.Content = JsonSerializer.Serialize(content);
+    this.StatusCode = (int)statusCode;
+  }
 }
